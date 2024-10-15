04e31405d7485549e937482e88f4f210b004dce5
ragulbarathi <ragulbarathi98@gmail.com>
2024-10-15 19:08:37 +0530

    Added New Selenium project Added Readme file Added few scripts for herokurapp

---
+++ readme.md
@@ -0,0 +1,10 @@
+Selenium With Python
+
+Tech stack
+Python 3.125
+allure 
+selenium 4.25
+faker
+pandas
+pytest
+pytest -xdist
---
+++ src/oct/15Oct2024/test_verifyTitle.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import allure
+import pytest
+
+
+def test_verify_title():
+    driver = webdriver.Chrome
+    driver.get("https://katalon-demo-cura.herokuapp.com/")
+    assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/"
+    assert driver.title == "CURA Healthcare Service"
